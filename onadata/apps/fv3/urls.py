508163f44c4ef4d3d4c3268b6f93e302c4d73963
awemulya <awemulya@gmail.com>
2019-05-09 10:21:30 +0545

    url clean

--- onadata/apps/fv3/urls.py
+++ onadata/apps/fv3/urls.py
@@ -4,7 +4,7 @@ from onadata.apps.fv3.views import supervisor_projects, MySuperviseSitesViewset,
 
 urlpatterns = [
 
-    url(r'^api/supervisor/projects/', supervisor_projects, name='supervisor_projects'),
-    url(r'^api/supervisor/sites/', MySuperviseSitesViewset.as_view({'get': 'list'}),  name='supervisor_sites'),
+    url(r'^api/projects/', supervisor_projects, name='supervisor_projects'),
+    url(r'^api/sites/', MySuperviseSitesViewset.as_view({'get': 'list'}),  name='supervisor_sites'),
     url(r'^api/site/blueprint/', site_blueprints, name='site_blueprints'),
     ]
