4c6aaac24c61b8997003fc302994aee175bb9b7f
aasis <aashish.baidya.c3@gmail.com>
2019-05-14 22:56:02 +0545

    bug in user query fixed..

--- onadata/apps/fieldsight/tasks.py
+++ onadata/apps/fieldsight/tasks.py
@@ -2089,7 +2089,7 @@ def exportProjectUserstatistics(task_prog_obj_id, source_user, project_id, start
             "sites_visited": 0
         }
 
-        users=User.objects.filter(user_roles__project_id=project_id, user_roles__group_id__in=[4, 9], ended_at__isnull=True).distinct('id').values('id')
+        users=User.objects.filter(user_roles__project_id=project_id, user_roles__group_id__in=[4, 9]).distinct('id').values('id')
 
         for user in User.objects.filter(pk__in=users).annotate(**query):
             data.append([user.username, user.get_full_name(), user.email, user_stats.get(user.username, dumb_visits)['submissions'], user_stats.get(user.username, dumb_visits)['sites_visited'], user_stats.get(user.username, dumb_visits)['total_worked_days'], user.monthly, user.weekly, user.daily])
