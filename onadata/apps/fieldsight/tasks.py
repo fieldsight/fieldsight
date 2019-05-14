1166c557981cec317841ee9d37d45312979901e4
aasis <aashish.baidya.c3@gmail.com>
2019-05-14 23:18:28 +0545

    bug in user query fixed..

--- onadata/apps/fieldsight/tasks.py
+++ onadata/apps/fieldsight/tasks.py
@@ -2096,7 +2096,7 @@ def exportProjectUserstatistics(task_prog_obj_id, source_user, project_id, start
 
         wb = Workbook()
         ws = wb.active
-        sheet_name = "Report "+start_date+"-"+end_date
+        sheet_name = "Report "+start_date+"_"+end_date
         sheet_name = sheet_name[:30]
         for ch in ["[", "]", "*", "?", ":", "/"]:
             if ch in sheet_name:
