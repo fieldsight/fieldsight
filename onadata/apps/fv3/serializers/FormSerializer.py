879b81b26833a65885e307fe4f2ff98ac53223d3
awemulya <awemulya@gmail.com>
2019-06-05 16:19:06 +0545

    forms library download preview edit data

--- onadata/apps/fv3/serializers/FormSerializer.py
+++ onadata/apps/fv3/serializers/FormSerializer.py
@@ -4,6 +4,22 @@ from onadata.apps.logger.models import XForm
 
 
 class XFormSerializer(serializers.ModelSerializer):
+    edit_url = serializers.SerializerMethodField()
+    preview_url = serializers.SerializerMethodField()
+    replace_url = serializers.SerializerMethodField()
+    download_url = serializers.SerializerMethodField()
     class Meta:
         model = XForm
-        fields=('id_string','title')
+        fields=('id_string','title', 'edit_url', 'preview_url', 'replace_url', 'download_url')
+
+    def get_edit_url(self, obj):
+        return obj.id_string
+
+    def get_preview_url(self, obj):
+        return obj.id_string
+
+    def get_replace_url(self, obj):
+        return obj.id_string
+
+    def get_download_url(self, obj):
+        return obj.id_string
--- onadata/apps/fv3/viewsets/FormsViewset.py
+++ onadata/apps/fv3/viewsets/FormsViewset.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 
+from onadata.apps.fieldsight.models import Project
 from onadata.apps.fsforms.models import FieldSightXF
 from onadata.apps.fv3.serializers.FormSerializer import XFormSerializer
 from onadata.apps.logger.models import XForm
@@ -29,7 +30,13 @@ class MyProjectFormsViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         projects = self.request.roles.filter(
-            ended_at__isnull=False).values_list("project", flat=True).order_by('project').distinct('project')
-        forms = FieldSightXF.objects.filter(
-            project__in=projects).exclude(xf__user=self.request.user).order_by("xf").values('xf').distinct()
-        return forms
+            ended_at__isnull=True).values_list("project", flat=True).order_by('project').distinct()
+        organizations = self.request.roles.filter(
+            ended_at__isnull=True, group__name="Organization Admin").values_list(
+            "organization", flat=True)
+        admin_projects = Project.objects.filter(organization__pk__in=organizations).values_list("pk", flat=True)
+        projects = [p for p in projects] + [p for p in admin_projects]
+        fieldsight_forms = FieldSightXF.objects.filter(
+            project__in=projects).exclude(xf__user=self.request.user).order_by("xf").distinct()
+        results = [f.xf for f in fieldsight_forms]
+        return results
