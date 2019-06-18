9a411e63ddda50d49fb0176bc1c6e4c4d32805ff
SanipRijal <saneprijal@gmail.com>
2019-06-18 13:13:50 +0545

    fix not form not shared

--- onadata/apps/fsforms/models.py
+++ onadata/apps/fsforms/models.py
@@ -380,11 +380,8 @@ def create_messages(sender, instance, created,  **kwargs):
                                                      description="Share Forms",
                                                      task_type=17, content_object=instance)
         if task_obj:
-            try:
-                with transaction.atomic():
-                    share_form_managers.apply_async(kwargs={'fxf': instance.id, 'task_id': task_obj.id}, countdown=5)
-            except IntegrityError as e:
-                print(e)
+            share_form_managers.delay(instance.id, task_obj.id)
+
 
 @receiver(pre_delete, sender=FieldSightXF)
 def send_delete_message(sender, instance, using, **kwargs):
