9e67eceb6564a949c7e453ccba25c802e1f04638
aasis <aashish.baidya.c3@gmail.com>
2019-06-18 22:37:51 +0545

    used only instead of values in queryset.

--- onadata/apps/fsforms/models.py
+++ onadata/apps/fsforms/models.py
@@ -124,7 +124,7 @@ class Stage(models.Model):
         if not self.stage:
             qs= Stage.objects.select_related('stage_forms__xf').filter(stage=self)
             if sync_details:
-                return qs.filter(stage_forms__sync_schedule__isnull=False).values('stage_forms__id','name','stage_id', 'stage_forms__xf__id_string', 'stage_forms__xf__user__username', 'stage_forms__sync_schedule__id', 'stage_forms__sync_schedule__schedule', 'stage_forms__sync_schedule__day')
+                return qs.filter(stage_forms__sync_schedule__isnull=False).only('stage_forms__id','name','stage_id', 'stage_forms__xf__id_string', 'stage_forms__xf__user__username', 'stage_forms__sync_schedule__id', 'stage_forms__sync_schedule__schedule', 'stage_forms__sync_schedule__day')
 
             return qs.values('stage_forms__id','name','stage_id', 'stage_forms__xf__id_string', 'stage_forms__xf__user__username')
         return []
