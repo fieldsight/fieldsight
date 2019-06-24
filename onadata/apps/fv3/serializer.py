7a2de6b69d7c3339721f254b456ebcf659ab7074
raeesaroj <raesaroj16@gmail.com>
2019-06-24 17:12:11 +0545

    base64 image in drf file upload

--- onadata/apps/fv3/serializer.py
+++ onadata/apps/fv3/serializer.py
@@ -1,9 +1,48 @@
+import base64
+import imghdr
+import uuid
+
+import six
+from django.core.files.base import ContentFile
 from rest_framework import serializers
 
 
 from onadata.apps.fieldsight.models import Project, Organization, Region, Site, Sector, SiteType,  ProjectLevelTermsAndLabels
 from onadata.apps.fieldsight.serializers.SiteSerializer import SiteTypeSerializer
 
+
+class Base64ImageField(serializers.ImageField):
+
+    def to_internal_value(self, data):
+
+        # Check if this is a base64 string
+        if isinstance(data, six.string_types):
+            # Check if the base64 string is in the "data:" format
+            if 'data:' in data and ';base64,' in data:
+                header, data = data.split(';base64,')
+
+            # Try to decode the file. Return validation error if it fails.
+            try:
+                decoded_file = base64.b64decode(data)
+            except TypeError:
+                self.fail('invalid_image')
+
+            # Generate file name:
+            file_name = str(uuid.uuid4())[:12] # 12 characters are more than enough.
+            # Get the file name extension:
+            file_extension = self.get_file_extension(file_name, decoded_file)
+
+            complete_file_name = "%s.%s" % (file_name, file_extension, )
+
+            data = ContentFile(decoded_file, name=complete_file_name)
+
+        return super(Base64ImageField, self).to_internal_value(data)
+
+    def get_file_extension(self, file_name, decoded_file):
+        extension = imghdr.what(file_name, decoded_file)
+        extension = "jpg" if extension == "jpeg" else extension
+
+        return extension
 
 
 class OrganizationSerializer(serializers.ModelSerializer):
@@ -72,11 +111,16 @@ class ProjectSerializer(serializers.ModelSerializer):
 
 
 class ProjectUpdateSerializer(serializers.ModelSerializer):
+    logo = Base64ImageField(
+        max_length=None, use_url=True, allow_empty_file=True
+    )
    
     class Meta:
         model = Project
         fields = ('id', 'name', 'phone', 'email', 'address', 'website', 'donor', 'public_desc', 'logo',
                   'location', 'cluster_sites', 'sector', 'sub_sector', 'organization')
+
+        extra_kwargs = {'logo': {'required': False}}
 
 
 class SectorSerializer(serializers.ModelSerializer):
