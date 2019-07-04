e7da2dfeca3bdfb9ae43473e020249c0bfa1dc66
raeesaroj <raesaroj16@gmail.com>
2019-07-04 09:35:03 +0545

    has_region field in project site list api

--- onadata/apps/fv3/viewsets/ProjectSitesListViewset.py
+++ onadata/apps/fv3/viewsets/ProjectSitesListViewset.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from onadata.apps.fieldsight.models import Site
+from onadata.apps.fieldsight.models import Site, Project
 from onadata.apps.fv3.serializers.ProjectSitesListSerializer import ProjectSitesListSerializer
 
 
@@ -23,3 +24,22 @@ class ProjectSitesListViewSet(viewsets.ReadOnlyModelViewSet):
         if project_id is not None:
 
             return self.queryset.filter(project_id=project_id, is_survey=False, is_active=True)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            data = list(serializer.data)
+            project_id = self.request.query_params.get('project', None)
+            obj = Project.objects.get(id=project_id)
+            if obj.cluster_sites:
+                data.append({"has_region": True})
+            else:
+                data.append({"has_region": False})
+            return self.get_paginated_response(data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
