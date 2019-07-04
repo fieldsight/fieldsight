66df9c13c58fd9ff173363f5cc37f2024b24c3e5
raeesaroj <raesaroj16@gmail.com>
2019-07-04 10:27:43 +0545

    migrations conflict in fieldsight and fsforms app

---
+++ onadata/apps/fieldsight/migrations/0088_merge.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fieldsight', '0084_merge'),
+        ('fieldsight', '0082_project_gsuit_sync_day'),
+        ('fieldsight', '0087_progresssettings_date'),
+    ]
+
+    operations = [
+    ]
---
+++ onadata/apps/fsforms/migrations/0077_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fsforms', '0076_auto_20190620_1753'),
+        ('fsforms', '0076_syncschedule_day'),
+    ]
+
+    operations = [
+    ]
