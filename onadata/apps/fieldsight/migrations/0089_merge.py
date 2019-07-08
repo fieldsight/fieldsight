4b69a342b665e33cc8d10682be4ed8af4b3702d8
raeesaroj <raesaroj16@gmail.com>
2019-07-08 10:00:13 +0545

    migrations merge added

---
+++ onadata/apps/fieldsight/migrations/0089_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fieldsight', '0084_auto_20190708_0618'),
+        ('fieldsight', '0088_merge'),
+    ]
+
+    operations = [
+    ]
---
+++ onadata/apps/fsforms/migrations/0079_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fsforms', '0077_merge'),
+        ('fsforms', '0078_merge'),
+    ]
+
+    operations = [
+    ]
