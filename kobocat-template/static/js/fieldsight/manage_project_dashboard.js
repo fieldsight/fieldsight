3abf902e413660de273d3dd6cc30cf351caab352
SanipRijal <saneprijal@gmail.com>
2019-07-17 10:18:31 +0545

    cleanup

--- kobocat-template/static/js/fieldsight/manage_project_dashboard.js
+++ kobocat-template/static/js/fieldsight/manage_project_dashboard.js
@@ -589,7 +589,7 @@ window.exportSitesToProject = new Vue({
       }
       
       function successCallback(response) {
-          alert(response.body.message)
+          alert(response.body.message);
           self.loading = false;
       }
       function errorCallback() {
