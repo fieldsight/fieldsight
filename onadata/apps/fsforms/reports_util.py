f0298ad24ae1d4051629503d0151bbac90306c1a
awemulya <awemulya@gmail.com>
2019-07-23 14:45:02 +0545

    all images remove deleted submissions

--- onadata/apps/fsforms/reports_util.py
+++ onadata/apps/fsforms/reports_util.py
@@ -23,8 +23,15 @@ def get_images_for_site(site_id):
             {"$match":{"_attachments.mimetype" : "image/jpeg"}},
             { "$sort" : { "_id": -1 }}, { "$limit": 6 }])
 
+
 def get_images_for_site_all(site_id):
-    return settings.MONGO_DB.instances.aggregate([{"$match":{"fs_site": {'$in' : [str(site_id), int(site_id)]}}}, {"$unwind":"$_attachments"},{"$match":{"_attachments.mimetype" : "image/jpeg"}}, { "$sort" : { "_id": -1 }}])
+    return settings.MONGO_DB.instances.aggregate(
+        [{"$match": {
+            "fs_site": {'$in': [str(site_id), int(site_id)]},
+            '_deleted_at': {'$exists': False}}},
+            {"$unwind": "$_attachments"},
+            {"$match": {"_attachments.mimetype": "image/jpeg"}},
+            {"$sort": {"_id": -1}}])
 
 def get_site_responses_coords(site_id):
     return settings.MONGO_DB.instances.aggregate([{"$match":{"fs_site": {'$in' : [str(site_id), int(site_id)]}, "_geolocation":{"$not":{ "$elemMatch": { "$eq": None }}}}}, {"$project" : {"_id":0, "type": {"$literal": "Feature"}, "geometry":{ "type": {"$literal": "Point"}, "coordinates": "$_geolocation" }, "properties": {"id":"$_id", "fs_uuid":"$fs_uuid", "submitted_by":"$_submitted_by"}}}])
