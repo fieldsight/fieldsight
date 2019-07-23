457da7c35e5adf80f318ed4c4e17ae4b47f0dba0
SanipRijal <saneprijal@gmail.com>
2019-07-23 14:28:56 +0545

    reduce sleep time in update meta information

--- onadata/apps/fieldsight/tasks.py
+++ onadata/apps/fieldsight/tasks.py
@@ -2590,7 +2590,7 @@ def create_site_meta_attribs_ans_history(pk, task_id):
     try:
         sites = Site.objects.filter(project_id=pk)
         for site in sites:
-            time.sleep(3)
+            time.sleep(1)
             metas = get_site_meta_ans(site.id)
             if metas == site.site_meta_attributes_ans:
                 continue
