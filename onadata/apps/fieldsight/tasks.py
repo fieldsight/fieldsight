db56eb48744395726074e1433c3d35f6726a09cb
SanipRijal <saneprijal@gmail.com>
2019-07-24 17:07:53 +0545

    change in site featured images format

--- onadata/apps/fieldsight/tasks.py
+++ onadata/apps/fieldsight/tasks.py
@@ -2617,9 +2617,7 @@ def update_meta_details(fs_proj_xf_id, instance_id, task_id, site_id):
             question_name = site_picture['question'].get('name', '')
             logo_url = instance.json.get(question_name)
             if logo_url:
-                site.logo.name = instance.json.get('_submitted_by') + '/attachments/' +\
-                                 instance.json.get('formhub/uuid') + '/' + \
-                                 instance.json.get('meta/instanceID').split(':')[1] + '/' + logo_url
+                site.logo.name = logo_url
 
         site_loc = fs_proj_xf.project.site_basic_info.get('site_location', None)
         if site_loc and site_loc.get('question_type', '') == 'Form' and site_loc.get('form_id', 0) == fs_proj_xf.id and site_loc.get('question', {}):
@@ -2634,10 +2632,12 @@ def update_meta_details(fs_proj_xf_id, instance_id, task_id, site_id):
                 question_name = featured_img['question'].get('name', '')
                 logo_url = instance.json.get(question_name)
                 if logo_url:
-                    site.site_featured_images[question_name] = instance.json.get('_submitted_by') + '/attachments/' + \
-                                                               instance.json.get('formhub/uuid') + '/' + \
-                                                               instance.json.get('meta/instanceID').split(':')[1] + \
-                                                               '/' + logo_url
+                    attachments = {}
+                    attachments['_attachments'] = instance.json.get('_submitted_by') + '/attachments/' + \
+                                                  instance.json.get('formhub/uuid') + \
+                                                  instance.json.get('meta/instanceID').split(':')[1] + '/' + logo_url
+                    attachments['_id'] = fs_proj_xf_id
+                    site.site_featured_images[question_name] = attachments
 
         # change site meta attributes answer
         meta_ans = site.site_meta_attributes_ans
