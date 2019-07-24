b36b307b172b3c3a7b8405c31e9b12c92088c5a0
SanipRijal <saneprijal@gmail.com>
2019-07-24 17:25:02 +0545

    change in image url for site logo

--- onadata/apps/fieldsight/tasks.py
+++ onadata/apps/fieldsight/tasks.py
@@ -2617,7 +2617,7 @@ def update_meta_details(fs_proj_xf_id, instance_id, task_id, site_id):
             question_name = site_picture['question'].get('name', '')
             logo_url = instance.json.get(question_name)
             if logo_url:
-                site.logo.name = logo_url
+                site.logo.url = 'https://app.fieldsight.org/attachment/medium?media_file=' + instance.json.get('_submitted_by') + '/attachments/' + logo_url
 
         site_loc = fs_proj_xf.project.site_basic_info.get('site_location', None)
         if site_loc and site_loc.get('question_type', '') == 'Form' and site_loc.get('form_id', 0) == fs_proj_xf.id and site_loc.get('question', {}):
@@ -2636,7 +2636,7 @@ def update_meta_details(fs_proj_xf_id, instance_id, task_id, site_id):
                     attachments['_attachments'] = instance.json.get('_submitted_by') + '/attachments/' + \
                                                   instance.json.get('formhub/uuid') + '/' + \
                                                   instance.json.get('meta/instanceID').split(':')[1] + '/' + logo_url
-                    attachments['_id'] = fs_proj_xf_id
+                    attachments['_id'] = instance.id
                     site.site_featured_images[question_name] = attachments
 
         # change site meta attributes answer
