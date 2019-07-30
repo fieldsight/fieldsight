1f2c0fd7d7c2ddcd07eeaa106d32000c7d54c4f7
SanipRijal <saneprijal@gmail.com>
2019-07-30 10:15:49 +0545

    Management command to check invalid root tag in forms

---
+++ onadata/apps/fsforms/management/commands/get_incorrect_root_tag_form.py
@@ -0,0 +1,39 @@
+import os
+from django.core.management.base import BaseCommand
+from onadata.apps.logger.models import Instance, XForm
+from onadata.apps.fsforms.models import XformHistory
+import re
+
+
+class Command(BaseCommand):
+    help = 'Find the forms that contain incorrect root tag in xml'
+
+    # def add_arguments(self, parser):
+    #     parser.add_argument('username', type=str)
+
+    def handle(self, *args, **options):
+        # for every forms
+        total_xf = XForm.objects.all().count()
+        page_size = 100
+        page = 0
+        while total_xf > 100:
+            xf = XForm.objects.all()[page*page_size:(page+1)*page_size]
+            print('Checking xform from ', page * page_size, (page + 1) * page_size)
+            for form in xf:
+                id_string = form.id_string
+                xml = form.xml
+                if id_string + '_' in xml:
+                    print('Xform: ', form.id, form.id_string, form.version)
+
+        # for  ever forms history
+        total_xfhist = XformHistory.objects.all().count()
+        page_size = 100
+        page = 0
+        while total_xfhist > 100:
+            xfhist = XformHistory.objects.all()[page*page_size:(page+1)*page_size]
+            print('Checking xform history from ', page * page_size, (page + 1) * page_size)
+            for form in xfhist:
+                id_string = form.id_string
+                xml = form.xml
+                if id_string + '_' in xml:
+                    print('Xform history: ', form.id, form.id_string, form.version)
