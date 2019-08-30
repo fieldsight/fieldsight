7bae189b98d2140d51b356c8f64c5d11124d00b0
raeesaroj <raesaroj16@gmail.com>
2019-08-30 13:56:02 +0545

    breadcrumbs in team-projects.

--- onadata/apps/fv3/viewsets/TeamViewSet.py
+++ onadata/apps/fv3/viewsets/TeamViewSet.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 
 from onadata.apps.fv3.serializers.TeamSerializer import TeamSerializer, TeamProjectSerializer
 from onadata.apps.fieldsight.models import Organization, Project
@@ -27,3 +28,12 @@ class TeamProjectsViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         return self.queryset.filter(organization=self.kwargs.get('pk'), is_active=True)
+
+    def list(self, request, *args, **kwargs):
+        projects = self.serializer_class(self.get_queryset(), many=True).data
+        team = Organization.objects.get(id=self.kwargs.get('pk'))
+
+        data = {'projects': projects, 'breadcrumbs': {'name': 'Projects', 'team': team.name,
+                                                      'team_url': team.get_absolute_url()}}
+
+        return Response(data)
