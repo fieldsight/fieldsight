85a198eb9a9ea1499accaf044f873ee3ec2cc664
raeesaroj <raesaroj16@gmail.com>
2019-09-03 09:55:52 +0545

    check invitations if new user have.

--- onadata/apps/users/views.py
+++ onadata/apps/users/views.py
@@ -322,7 +322,10 @@ class ProfileCreateView(MyProfileView, CreateView):
         user.save()
         form.instance.user = self.request.user
         self.object = form.save()
-        accept_all_invitations(self.request, user.username)
+        invitations = UserInvite.objects.filter(email=user.email, is_used=False, is_declied=False).exists()
+        if invitations:
+
+            accept_all_invitations(self.request, user.username)
         # return HttpResponseRedirect(reverse_lazy('users:view_invitations', kwargs={'pk':user.pk}))
         return HttpResponseRedirect('/fieldsight/application/#/my-roles')
 
