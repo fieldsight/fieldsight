84f0bee7a995cfef30f9627645d0557c7ea464af
raeesaroj <raesaroj16@gmail.com>
2019-09-05 13:43:34 +0545

    stripe subscriptions Apiview.

--- onadata/apps/fv3/viewsets/TeamViewSet.py
+++ onadata/apps/fv3/viewsets/TeamViewSet.py
@@ -10,12 +10,15 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authentication import SessionAuthentication
 
 from onadata.apps.fv3.serializers.TeamSerializer import TeamSerializer, TeamProjectSerializer
 from onadata.apps.fieldsight.models import Organization, Project
 from onadata.apps.fv3.role_api_permissions import TeamDashboardPermissions
 from onadata.apps.subscriptions.models import Customer, Package, Subscription
 from onadata.apps.subscriptions.views import MONTHLY_PLAN_NAME, YEARLY_PLAN_NAME
+from onadata.apps.fsforms.enketo_utils import CsrfExemptSessionAuthentication
 
 
 class TeamDashboardViewSet(viewsets.ReadOnlyModelViewSet):
@@ -50,80 +53,83 @@ class TeamProjectsViewSet(viewsets.ReadOnlyModelViewSet):
         return Response(data)
 
 
-@api_view(['POST'])
-def subscriptions(request, org_id):
-    try:
-        organization = Organization.objects.get(id=org_id)
+class StripeSubscriptions(APIView):
 
-    except ObjectDoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    authentication_classes = (CsrfExemptSessionAuthentication, SessionAuthentication, IsAuthenticated)
 
-    customer_data = {
-        'email': request.user.email,
-        'description': 'Some Customer Data',
-        'card': request.POST['stripeToken'],
-        'metadata': {'username': request.user.username}
-    }
+    def post(self, request, org_id, format=None):
+        try:
+            organization = Organization.objects.get(id=org_id)
 
-    customer = stripe.Customer.create(**customer_data)
-    cust = Customer.objects.get(user=request.user)
-    cust.stripe_cust_id = customer.id
-    cust.save()
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    stripe_customer = stripe.Customer.retrieve(cust.stripe_cust_id)
-    card = stripe_customer.sources.data[0].last4
+        customer_data = {
+            'email': request.user.email,
+            'description': 'Some Customer Data',
+            'card': request.POST['stripeToken'],
+            'metadata': {'username': request.user.username}
+        }
 
-    period = request.POST['interval']
+        customer = stripe.Customer.create(**customer_data)
+        cust = Customer.objects.get(user=request.user)
+        cust.stripe_cust_id = customer.id
+        cust.save()
 
-    starting_date = datetime.now().strftime('%A, %B %d, %Y')
-    if period == 'yearly':
-        overage_plan = settings.YEARLY_PLANS_OVERRAGE[request.POST['plan_name']]
-        selected_plan = settings.YEARLY_PLANS[request.POST['plan_name']]
-        package = Package.objects.get(plan=settings.PLANS[selected_plan], period_type=2)
-        ending_date = datetime.now() + dateutil.relativedelta.relativedelta(months=12)
-        plan_name = YEARLY_PLAN_NAME[request.POST['plan_name']]
+        stripe_customer = stripe.Customer.retrieve(cust.stripe_cust_id)
+        card = stripe_customer.sources.data[0].last4
 
-    elif period == 'monthly':
-        overage_plan = settings.MONTHLY_PLANS_OVERRAGE[request.POST['plan_name']]
-        selected_plan = settings.MONTHLY_PLANS[request.POST['plan_name']]
-        package = Package.objects.get(plan=settings.PLANS[selected_plan], period_type=1)
-        ending_date = datetime.now() + dateutil.relativedelta.relativedelta(months=1)
-        plan_name = MONTHLY_PLAN_NAME[request.POST['plan_name']]
+        period = request.POST['interval']
 
-    try:
+        starting_date = datetime.now().strftime('%A, %B %d, %Y')
+        if period == 'yearly':
+            overage_plan = settings.YEARLY_PLANS_OVERRAGE[request.POST['plan_name']]
+            selected_plan = settings.YEARLY_PLANS[request.POST['plan_name']]
+            package = Package.objects.get(plan=settings.PLANS[selected_plan], period_type=2)
+            ending_date = datetime.now() + dateutil.relativedelta.relativedelta(months=12)
+            plan_name = YEARLY_PLAN_NAME[request.POST['plan_name']]
 
-        sub = customer.subscriptions.create(
-            items=[
-                {
-                    'plan': selected_plan,
+        elif period == 'monthly':
+            overage_plan = settings.MONTHLY_PLANS_OVERRAGE[request.POST['plan_name']]
+            selected_plan = settings.MONTHLY_PLANS[request.POST['plan_name']]
+            package = Package.objects.get(plan=settings.PLANS[selected_plan], period_type=1)
+            ending_date = datetime.now() + dateutil.relativedelta.relativedelta(months=1)
+            plan_name = MONTHLY_PLAN_NAME[request.POST['plan_name']]
 
-                },
+        try:
 
-                {
-                    'plan': overage_plan,
-                },
+            sub = customer.subscriptions.create(
+                items=[
+                    {
+                        'plan': selected_plan,
 
-            ],
+                    },
 
-        )
-    except Exception as e:
-        return Response(status=status.HTTP_404_NOT_FOUND, data={'error': 'No such plan in stripe.'})
-    sub_data = {
-        'stripe_sub_id': sub.id,
-        'is_active': True,
-        'initiated_on': datetime.now(),
-        'package': Package.objects.get(plan=settings.PLANS[selected_plan]),
-        'organization': organization
+                    {
+                        'plan': overage_plan,
+                    },
 
-    }
-    try:
-        # Subscription.objects.create(**sub_data)
-        Subscription.objects.filter(stripe_customer=cust, stripe_sub_id="free_plan").update(**sub_data)
+                ],
 
-    except Exception as e:
-        return Response(status=status.HTTP_204_NO_CONTENT, data={'error': str(e)})
+            )
+        except Exception as e:
+            return Response(status=status.HTTP_404_NOT_FOUND, data={'error': 'No such plan in stripe.'})
+        sub_data = {
+            'stripe_sub_id': sub.id,
+            'is_active': True,
+            'initiated_on': datetime.now(),
+            'package': Package.objects.get(plan=settings.PLANS[selected_plan]),
+            'organization': organization
 
-    return Response(status=status.HTTP_201_CREATED, data={'organization': organization,
+        }
+        try:
+            # Subscription.objects.create(**sub_data)
+            Subscription.objects.filter(stripe_customer=cust, stripe_sub_id="free_plan").update(**sub_data)
+
+        except Exception as e:
+            return Response(status=status.HTTP_204_NO_CONTENT, data={'error': str(e)})
+
+        return Response(status=status.HTTP_201_CREATED, data={'organization': organization,
                                                               'submissions': package.submissions,
                                                               'amount': package.total_charge,
                                                               'starting_date': starting_date,
