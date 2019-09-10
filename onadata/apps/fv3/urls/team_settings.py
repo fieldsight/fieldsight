7f9ca9fe5a4230a6b7108e229c370d21eee4f975
raeesaroj <raesaroj16@gmail.com>
2019-09-10 16:52:29 +0545

    team owner account information api.

--- onadata/apps/fv3/urls/team_settings.py
+++ onadata/apps/fv3/urls/team_settings.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from onadata.apps.fv3.viewsets.TeamSettingsViewset import TeamViewset, team_types_countries, TeamGeoLayerViewset
+from onadata.apps.fv3.viewsets.TeamSettingsViewset import TeamViewset, team_types_countries, TeamGeoLayerViewset, \
+    TeamOwnerAccount
 
 router = routers.DefaultRouter()
 
@@ -10,6 +11,7 @@ router.register(r'team-geolayer', TeamGeoLayerViewset, base_name='team_geolayer'
 team_settings_urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^api/team-types-countries', team_types_countries, name="team_types"),
+    url(r'^api/team-owner-account/(?P<org_id>\d+)/$', TeamOwnerAccount.as_view(), name="team_owner_account"),
 
 ]
 
--- onadata/apps/fv3/viewsets/TeamSettingsViewset.py
+++ onadata/apps/fv3/viewsets/TeamSettingsViewset.py
@@ -1,3 +1,6 @@
+import stripe
+from django.conf import settings
+
 from django.contrib.gis.geos import Point
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -6,11 +9,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework import viewsets, status
+from rest_framework.views import APIView
 
 from onadata.apps.fieldsight.models import Organization, OrganizationType, COUNTRIES
 from onadata.apps.fv3.serializers.TeamSerializer import TeamUpdateSerializer, TeamGeoLayer
 from onadata.apps.fsforms.enketo_utils import CsrfExemptSessionAuthentication
 from onadata.apps.geo.models import GeoLayer
+from onadata.apps.subscriptions.models import Customer, Subscription
 
 
 class TeamViewset(viewsets.ModelViewSet):
@@ -74,6 +79,68 @@ class TeamGeoLayerViewset(viewsets.ModelViewSet):
         self.perform_update(serializer)
 
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+
+class TeamOwnerAccount(APIView):
+
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, *args,  **kwargs):
+
+        try:
+            team = Organization.objects.get(id=self.kwargs.get('org_id'))
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND, data={"detail": "Not found."})
+
+        if request.user == team.owner:
+
+            try:
+                customer = Customer.objects.get(user=self.request.user)
+                email = customer.user.email
+            except ObjectDoesNotExist:
+                return Response(status=status.HTTP_404_NOT_FOUND, data={"detail": "Not found."})
+            if not customer.stripe_cust_id == 'free_cust_id':
+                stripe_customer = stripe.Customer.retrieve(customer.stripe_cust_id)
+                card = stripe_customer.sources.data[0].last4
+            else:
+                card = ''
+                email = ''
+
+            subscribed_package = Subscription.objects.select_related('package', 'stripe_customer').get(stripe_customer=customer)
+            has_free_package = Subscription.objects.filter(stripe_sub_id="free_plan", stripe_customer__user=self.request.user,
+                                                           organization=team).exists()
+            if has_free_package:
+                period_type = 'Year'
+
+            else:
+                period_type = subscribed_package.package.get_period_type_display()
+
+            return Response(status=status.HTTP_200_OK, data={'has_free_package': has_free_package, "team_owner": True,
+                                                             'key': settings.STRIPE_PUBLISHABLE_KEY
+            if not has_free_package else None, 'subscribed_package': {'plan': subscribed_package.package.get_plan_display(),
+                                                                                    'total_charge': subscribed_package.package.total_charge,
+                                                                                    'period_type': period_type,
+                 'total_submissions': subscribed_package.package.submissions}, 'account_information': {'card': card, 'email': email}
+                                                             if not has_free_package else None})
+        else:
+            return Response(status=status.HTTP_200_OK, data={"team_owner": False})
+
+    def post(self, request, format=None):
+        """
+          replace old card with new
+        """
+        try:
+            customer = Customer.objects.get(user=request.user).stripe_cust_id
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND, data={"detail": "Not found."})
+
+        stripe.Customer.modify(
+            customer,
+            source=request.POST['stripeToken'],
+        )
+
+        return Response(status=status.HTTP_200_OK, data={"detail": "successfully updated."})
 
 
 @permission_classes([IsAuthenticated])
