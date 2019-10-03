2546f90ee0a666ad281ede8fde503c60ff90ea91
Awemulya Acharya <awemulya@gmail.com>
2019-10-03 11:08:20 +0545

    clean up

--- onadata/apps/fv3/serializers/FormSerializer.py
+++ onadata/apps/fv3/serializers/FormSerializer.py
@@ -324,7 +324,8 @@ class StageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Stage
-        exclude = ('shared_level', 'group', 'ready', 'stage', 'date_modified', 'date_created', 'tags', 'regions')
+        exclude = ('shared_level', 'group', 'ready', 'stage', 'date_modified', 'date_created',
+                   'tags', 'regions', 'types', 'regions')
 
     # def get_substages(self, stage):
     #     stages = Stage.objects.filter(stage=stage, is_deleted=False,
