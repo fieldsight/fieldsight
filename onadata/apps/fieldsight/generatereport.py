88328a43d03f5b18000a2aaed4c86abe1b3e179c
Awemulya Acharya <awemulya@gmail.com>
2019-10-23 11:32:30 +0545

    clean up

--- onadata/apps/fieldsight/generatereport.py
+++ onadata/apps/fieldsight/generatereport.py
@@ -603,7 +603,7 @@ class PDFReport:
         forms = FieldSightXF.objects.select_related('xf').filter(pk__in=fs_ids, is_survey=False, is_deleted=False
                                                                  ).order_by('project', 'is_staged', 'is_scheduled', 'stage__order', 'stage__date_created'
                                                                  ).prefetch_related(Prefetch('site_form_instances',
-                                                                                             queryset=FInstance.objects.select_related('instance').filter(date__range=[new_startdate, new_enddate])), Prefetch('project_form_instances', queryset=FInstance.objects.select_related('instance').filter(site_id=site.id, date__range=[new_startdate, new_enddate]))).order_by('-is_staged', 'is_scheduled')
+                                                                                             queryset=FInstance.objects.select_related('instance').filter(date__range=[new_startdate, new_enddate])), Prefetch('project_form_instances', queryset=FInstance.objects.select_related('instance').filter(site_id=site.id, date__range=[new_startdate, new_enddate])))
         
         if not forms:
             elements.append(Paragraph("No Any Responses Yet.", styles['Heading5']))
