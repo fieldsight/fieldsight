3b2f9d16dee9e57245c94be8c5aca4b64bc22587
Awemulya Acharya <awemulya@gmail.com>
2019-11-15 13:47:07 +0545

    geojson create clean

--- onadata/apps/fieldsight/management/commands/create_geojson.py
+++ onadata/apps/fieldsight/management/commands/create_geojson.py
@@ -1,22 +1,20 @@
-from datetime import datetime
 from django.core.management.base import BaseCommand
-from onadata.apps.fieldsight.models import TimeZone, Project, ProjectGeoJSON
+from onadata.apps.fieldsight.models import Project, ProjectGeoJSON
+
 
 class Command(BaseCommand):
-    help = 'Create site data Geojson file backup for all projects'
-    
-    def handle(self, *args, **options):
-    	
-    	projects = Project.objects.filter(is_active = True)
-        new_projects = 0
-        for project in projects:
-        	try:
-        		projectGeoJSON = project.project_geojson
-        	except:
-        		projectGeoJSON = ProjectGeoJSON.objects.create(project_id = project.id)
-        		projectGeoJSON.save()
-        		new_projects+=1
+	help = 'Create site data Geojson file backup for all projects'
 
-        	projectGeoJSON.generate_new()
-        self.stdout.write('Created "%s " new projects geo data with success!' % (new_projects))
-        print datetime.now()
+
+def handle(self, *args, **options):
+	projects = Project.objects.filter(is_active = True)
+	new_projects = 0
+	for project in projects:
+		try:
+			project_jeojson = project.project_geojson
+		except:
+			project_jeojson = ProjectGeoJSON.objects.create(project_id=project.id)
+		project_jeojson.save()
+		new_projects += 1
+		project_jeojson.generate_new()
+	self.stdout.write('Created "%s " new projects geo data with success!' % new_projects)
