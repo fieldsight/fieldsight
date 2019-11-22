6cff6796191bfb6fb3f282f1763cbe2dc86c568e
awemulya <awemulya@gmail.com>
2019-11-22 13:17:45 +0545

    clean

--- onadata/apps/fieldsight/tasks.py
+++ onadata/apps/fieldsight/tasks.py
@@ -316,7 +316,8 @@ def generate_stage_status_report(task_prog_obj_id, project_id, site_type_ids, re
             site_dict[str(site_obj.id)] = {'visits':0,'site_status':'No Submission', 'latitude':site_obj.latitude,'longitude':site_obj.longitude}
 
         sites_status=FInstance.objects.filter(
-            project_fxf__project=project_id).order_by('site_id',
+            project_fxf__project=project_id,
+            project_fxf___is_staged=True).order_by('site_id',
                                                           '-id').distinct('site_id').values_list('site_id', 'form_status')
         
         for site_status in sites_status:
