e0be56f61fb97aa344ed4411e6b71ed50db79da7
awemulya <awemulya@gmail.com>
2019-12-03 10:05:33 +0545

    update sheet command

--- onadata/apps/fieldsight/management/commands/update_sheet.py
+++ onadata/apps/fieldsight/management/commands/update_sheet.py
@@ -1,3 +1,5 @@
+import time
+
 from pprint import pprint
 from oauth2client.service_account import ServiceAccountCredentials
 from googleapiclient import discovery
@@ -13,128 +15,70 @@ scope = ['https://spreadsheets.google.com/feeds',
 
 
 class Command(BaseCommand):
-    ''' This command replace string in xml '''
+    ''' This command update sheets in drive'''
 
     help = 'update sheet data in google'
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--spreadsheet_id',
-            type=str,
-            dest='spreadsheet_id',
-            help='spreadsheet_id google'
-        )
-        parser.add_argument(
-            '--grid_id',
-            type=int,
-            dest='grid_id',
-            help='grid_id google'
-        )
-        parser.add_argument(
-            '--project',
-            type=int,
-            dest='project',
-            help='project id'
-        )
-
-        parser.add_argument(
-            '--range',
-            type=str,
-            dest='range',
-            help='range of columns e.g. A1:B3 '
-        )
-        parser.add_argument(
-            '--form_id',
-            type=int,
-            dest='form_id',
-            help='fieldsight Form id'
-        )
-        parser.add_argument(
-            '--report_type',
-            type=str,
-            dest='report_type',
-            help='type of report'
-        )
-
     def handle(self, *args, **options):
-        spreadsheet_id = options["spreadsheet_id"]
-        grid_id = options["grid_id"]
-        project = options["project"]
-        range = options["range"]
-        form_id = options["form_id"]
-        report_type = options["report_type"]
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive',
+                 'https://www.googleapis.com/auth/spreadsheets']
+        from onadata.apps.fieldsight.sheet_list import SHEET_LIST
+        if SHEET_LIST:
+            credentials = ServiceAccountCredentials.from_json_keyfile_name(
+                'service_account.json', scope)
 
-        add_rows = {
-            "requests": [
-                {
-                    "appendDimension": {
-                        "dimension": "ROWS",
-                        "length": 10000,
-                        "sheetId": grid_id
-                    }
-                }
-            ],
-            "includeSpreadsheetInResponse": False
-        }
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(
-            'service_account.json', scope)
+            service = discovery.build('sheets', 'v4', credentials=credentials,
+                                      cache_discovery=False)
+            for sheet in SHEET_LIST:
+                time.sleep(3)
+                values = []
+                report_type = sheet.get('report_type')
+                project = sheet.get('project')
+                form_id = sheet.get('form_id')
+                spreadsheet_id = sheet.get('spreadsheet_id')
+                grid_id = sheet.get('grid_id')
+                sheet_range = sheet.get('range')
+                if report_type == "site_info":
+                    values = site_information(project)
+                elif report_type == "site_progress":
+                    values = progress_information(project)
+                elif report_type == "form":
+                    values = form_submission(form_id)
+                if len(values) >= 10000:
+                    total_sites = len(values)
+                    page_size = 10000
+                    page = 0
+                    while total_sites > 0:
+                        chunk = values[page * page_size:(page + 1) * page_size]
+                        range = 'A{0}:GZ50000'.format((page * page_size) + 1)
+                        pprint(range, "   ==========   range")
+                        body = {'data': [{'majorDimension': 'ROWS',
+                                          'range': range,
+                                          'values': chunk
+                                          }],
+                                'valueInputOption': 'RAW'}
 
-        service = discovery.build('sheets', 'v4', credentials=credentials)
+                        request = service.spreadsheets().values().batchUpdate(
+                            spreadsheetId=spreadsheet_id, body=body)
 
-        values = []
+                        response = request.execute()
+                        pprint(response)
+                        pprint("finished ,", sheet, page)
+                        total_sites -= page_size
+                        page += 1
+                else:
+                    body = {'data': [{'majorDimension': 'ROWS',
+                                      'range': sheet_range,
+                                      'values': values
+                                      }],
+                            'valueInputOption': 'RAW'}
 
-        if report_type == "site_info":
-            values = site_information(project)
-        elif report_type == "site_progress":
-            values = progress_information(project)
-        elif report_type == "form":
-            values = form_submission(form_id)
-        if len(values) >= 10000:
-            total_sites = len(values)
-            page_size = 10000
-            page = 0
-            while total_sites > 0:
-                chunk = values[page * page_size:(page + 1) * page_size]
-                range = 'A{0}:GZ50000'.format((page * page_size) + 1)
-                print(range, "   ==========   range")
-                body = {'data': [{'majorDimension': 'ROWS',
-                                  'range': range,
-                                  'values': chunk
-                                  }],
-                        'valueInputOption': 'RAW'}
+                    request = service.spreadsheets().values().batchUpdate(
+                        spreadsheetId=spreadsheet_id, body=body)
 
-                # request_a = service.spreadsheets().values().batchUpdate(
-                #     spreadsheetId=spreadsheet_id, body=add_rows)
-                #
-                # response_a = request_a.execute()
-                # pprint(response_a)
+                    response = request.execute()
+                    pprint(response)
 
-                request = service.spreadsheets().values().batchUpdate(
-                    spreadsheetId=spreadsheet_id, body=body)
-
-                response = request.execute()
-                pprint(response)
-
-                self.stderr.write(
-                    '\nFinished {} '.format(page * page_size, )
-                )
-                total_sites -= page_size
-                page += 1
-        else:
-            body = {'data': [{'majorDimension': 'ROWS',
-                              'range': range,
-                              'values': values
-                              }],
-                    'valueInputOption': 'RAW'}
-
-            request = service.spreadsheets().values().batchUpdate(
-                spreadsheetId=spreadsheet_id, body=body)
-
-            response = request.execute()
-            pprint(response)
-
-
-            self.stderr.write(
-                '\nFinished {} '.format(spreadsheet_id)
-            )
+                    pprint("finished ,", sheet)
 
