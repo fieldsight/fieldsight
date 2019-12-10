8767cc68e42595cfb62348e11e7c7bc2b855eee4
Yubaraj Poudel <yubarajpoudel708@gmail.com>
2019-12-10 15:58:23 +0545

    clean up

--- onadata/apps/fsforms/models.py
+++ onadata/apps/fsforms/models.py
@@ -475,7 +475,7 @@ class FormSettings(models.Model):
 @receiver(post_save, sender=FieldSightXF)
 def create_messages(sender, instance, created,  **kwargs):
     if instance.project and created:
-        sync_settings = ReportSyncSettings(project=instance, report_type="form", schedule_type=0)
+        sync_settings = ReportSyncSettings(project=instance.project, form=instance, report_type="form", schedule_type=0)
         sync_settings.save()
     if instance.project is not None and created and not instance.is_staged and not instance.is_scheduled:
         send_message_project_form(instance)
