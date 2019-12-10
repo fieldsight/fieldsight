5ac75c6e7c9cc402a4e1543566896c3385c5d547
raeesaroj <raesaroj16@gmail.com>
2019-12-10 11:02:25 +0545

    report sync settings list api update.

--- onadata/apps/fv3/urls/reports.py
+++ onadata/apps/fv3/urls/reports.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from onadata.apps.fv3.viewsets.ReportViewsets import ReportSyncSettingsViewSet, ProjectFormsViewSet
+from onadata.apps.fv3.viewsets.ReportViewsets import ReportSyncSettingsViewSet, ProjectFormsViewSet, \
+    ReportSyncSettingsList
 
 reports_urlpatterns = [
-    url(r'^api/report-sync-settings-list/$', ReportSyncSettingsViewSet.as_view({'get': 'list'}),
-        name='report_sync_settings_list'),
+    url(r'^api/report-sync-settings-list/$', ReportSyncSettingsList.as_view(), name='report_sync_settings_list'),
     url(r'^api/report-sync-settings/$', ReportSyncSettingsViewSet.as_view({'post': 'create'}),
         name='report_sync_settings'),
     url(r'^api/project-forms/(?P<pk>\d+)/$', ProjectFormsViewSet.as_view({'get': 'list'}), name='project_forms'),
