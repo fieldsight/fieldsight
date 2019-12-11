a355d8b2676e52ac25db8be4a67668285e824599
raeesaroj <raesaroj16@gmail.com>
2019-12-11 14:58:50 +0545

    permissions in report sync settings api.

--- onadata/apps/fv3/permissions/reports.py
+++ onadata/apps/fv3/permissions/reports.py
@@ -4,20 +4,57 @@ from onadata.apps.fieldsight.models import Project
 from onadata.apps.fsforms.models import ReportSyncSettings
 
 
+def check_manager_or_admin_perm(request, project_id):
+
+    if not (request.user and request.user.is_authenticated()):
+        return False
+    if request.is_super_admin:
+        return True
+
+    project = Project.objects.get(pk=project_id)
+    user_role_asorgadmin = request.roles.filter(
+        organization_id=project.organization_id,
+        group__name="Organization Admin")
+    if user_role_asorgadmin:
+        return True
+    user_role = request.roles.filter(project_id=project_id,
+                                     group__name="Project Manager")
+    if user_role:
+        return True
+    return False
+
+
 class ReportSyncPermission(permissions.BasePermission):
     """
-    Report sync permission only to Organization admin and project managers
+    Report sync/edit permission only to Organization admin and project managers
     """
 
     def has_permission(self, request, view):
-        if not (request.user and request.user.is_authenticated()):
-            return False
-        if request.is_super_admin:
-            return True
+
         sheet_id = view.kwargs.get('pk')
         sheet = ReportSyncSettings.objects.get(id=sheet_id)
 
         project_id = sheet.project_id
+        if project_id:
+            return check_manager_or_admin_perm(request, project_id)
+
+        return False
+
+
+class ReportSyncSettingsViewPermission(permissions.BasePermission):
+    """
+    Report view permission only to Organization admin , project managers and donors
+    """
+
+    def has_permission(self, request, view):
+
+        if not (request.user and request.user.is_authenticated()):
+            return False
+        if request.is_super_admin:
+            return True
+
+        project_id = request.query_params.get('project_id')
+
         if project_id:
             project = Project.objects.get(pk=project_id)
             user_role_asorgadmin = request.roles.filter(
@@ -26,8 +63,9 @@ class ReportSyncPermission(permissions.BasePermission):
             if user_role_asorgadmin:
                 return True
             user_role = request.roles.filter(project_id=project_id,
-                                             group__name="Project Manager")
+                                             group__name__in=["Project Manager", "Project Donor"])
             if user_role:
                 return True
+            return False
 
         return False
--- onadata/apps/fv3/urls/reports.py
+++ onadata/apps/fv3/urls/reports.py
@@ -4,7 +4,8 @@ from onadata.apps.fv3.viewsets.ReportViewsets import ReportSyncSettingsViewSet, 
 
 reports_urlpatterns = [
     url(r'^api/report-sync-settings-list/$', ReportSyncSettingsList.as_view(), name='report_sync_settings_list'),
-    url(r'^api/report-sync-settings-today/$', ReportSyncSettingsToday.as_view({'get':'list'}), name='report_sync_settings_today'),
+    url(r'^api/report-sync-settings-today/$', ReportSyncSettingsToday.as_view({'get':'list'}),
+        name='report_sync_settings_today'),
     url(r'^api/update-report-sync-settings/(?P<pk>\d+)/$', ReportSyncSettingsViewSet.as_view({'put': 'update'}),
         name='report_sync_settings'),
     url(r'^api/report-sync/(?P<pk>\d+)/$', ReportSyncView.as_view(), name='report_sync'),
