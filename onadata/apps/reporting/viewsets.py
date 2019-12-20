e5bb12e60eb3e0dd9470a67ca888eb8d17695931
raeesaroj <raesaroj16@gmail.com>
2019-12-20 17:15:35 +0545

    meta attributes added in metrics data api.

--- onadata/apps/reporting/viewsets.py
+++ onadata/apps/reporting/viewsets.py
@@ -411,10 +411,16 @@ def metrics_data(request, pk):
     project = get_object_or_404(Project, id=pk)
     meta_attributes = project.site_meta_attributes
     report_types = [{'id': rep_type[0], 'name': rep_type[1]} for rep_type in REPORT_TYPES]
-    # to_be_deleted_keys = ['question_placeholder', 'question_type', 'question_help']
     for meta in meta_attributes:
         meta['code'] = meta.pop('question_name')
         meta['label'] = meta.pop('question_text')
+        meta['type'] = meta.pop('question_type')
+
+        if 'question_placeholder' in meta:
+            del meta['question_placeholder']
+
+        if 'question_help' in meta:
+            del meta['question_help']
 
     metrics = []
     metrics.extend(METRICES_DATA)
