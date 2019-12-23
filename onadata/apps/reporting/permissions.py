81130f16f24d3ce30393ce5cac49056e40dc0714
raeesaroj <raesaroj16@gmail.com>
2019-12-23 16:36:19 +0545

    report update api.

--- onadata/apps/reporting/permissions.py
+++ onadata/apps/reporting/permissions.py
@@ -9,6 +9,28 @@ class ReportingProjectFormsPermissions(permissions.BasePermission):
     Object-level permission to only allow owners of an object to View it.
     """
 
+    def has_object_permission(self, request, view, obj):
+        if request.is_super_admin:
+            return True
+
+        project = obj.project
+
+        if project is not None:
+            organization_id = project.organization_id
+            user_role_org_admin = request.roles.filter(organization_id=organization_id,
+                                                       group__name="Organization Admin")
+
+            if user_role_org_admin:
+                return True
+
+            user_role_as_manager = request.roles.filter(project_id=project.id, group__name__in=["Project Manager",
+                                                                                                "Project Donor"])
+
+            if user_role_as_manager:
+                return True
+
+        return False
+
     def has_permission(self, request, view):
 
         if request.is_super_admin:
