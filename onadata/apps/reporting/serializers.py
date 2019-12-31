90b0bbb584634011296598f556581598188922de
raeesaroj <raesaroj16@gmail.com>
2019-12-31 16:06:36 +0545

    retrieve report api.

--- onadata/apps/reporting/serializers.py
+++ onadata/apps/reporting/serializers.py
@@ -26,6 +26,7 @@ class StageFormSerializer(serializers.ModelSerializer):
 
 class ReportSettingsSerializer(serializers.ModelSerializer):
     owner_full_name = serializers.SerializerMethodField(read_only=True)
+    attributes = serializers.JSONField()
 
     class Meta:
         model = ReportSettings
