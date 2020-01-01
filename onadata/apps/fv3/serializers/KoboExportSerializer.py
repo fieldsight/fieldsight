1409b4032ead7b15c04f01e9b0a0bab27d69b999
awemulya <awemulya@gmail.com>
2020-01-01 11:43:07 +0545

    export list fields

--- onadata/apps/fv3/serializers/KoboExportSerializer.py
+++ onadata/apps/fv3/serializers/KoboExportSerializer.py
@@ -6,5 +6,6 @@ class ExportSerializer(serializers.Serializer):
 
     class Meta:
         model = Export
-        exclude = ()
+        fields = ('id', 'filename', 'internal_status', 'created_on',)
+        read_only_fields = ('id', 'filename', 'internal_status', 'created_on',)
 
