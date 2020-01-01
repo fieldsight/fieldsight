f72a5fb14ede18d0ab92d2e8fb225e521b5b71b0
awemulya <awemulya@gmail.com>
2020-01-01 14:28:17 +0545

    site report multi form data

--- onadata/apps/reporting/utils/site_report.py
+++ onadata/apps/reporting/utils/site_report.py
@@ -128,7 +128,7 @@ def generate_form_information(form_id, question, df, df_sub_form_data):
 def site_report(project_id):
     selected_metas = ['Slip_Number', '3rd_Installment__CM_']
     form_metrics = {'form_id': 73732, 'metrices': []}
-    form_information = {'form_id': 73732, 'question': 'status_cbi/va/member_16_59', 'metrices': []}
+    form_information = [{'form_id': 73732, 'question': 'status_cbi/va/member_16_59', 'metrices': []}]
     query = Site.objects.filter(project_id=project_id).values(
         'id', 'identifier', 'name', 'current_progress', 'all_ma_ans')
     df = pd.DataFrame(list(query), columns=['id', 'identifier', 'name', 'current_progress', 'all_ma_ans'])
@@ -161,13 +161,15 @@ def site_report(project_id):
     query_submissions_form = FInstance.objects.filter(
        project_fxf__in=[form_information['form_id']]).select_related(
        'instance').values("pk", "site", 'project_fxf', "instance__json", "date")
-    df_submissions_form_answer = pd.DataFrame(list(query_submissions_form),
+    df_submissions_form_answers = pd.DataFrame(list(query_submissions_form),
                                              columns=["pk", 'site', 'project_fxf', 'instance__json', "date"])
+    df_submissions_form_answer = df_submissions_form_answers[df_submissions_form_answers.project_fxf == form_information['form_id']]
     df_submissions_form_answer = pd.concat([df_submissions_form_answer.drop('instance__json', axis=1),
                                  df_submissions_form_answer['instance__json'].apply(pd.Series)], axis=1)
 
     # form submission answer of a question
-    df = generate_form_information(form_information['form_id'], form_information['question'], df, df_submissions_form_answer)
+    for form_id in [form_information['form_id']]:
+        df = generate_form_information(form_id, form_information['question'], df, df_submissions_form_answer)
 
     query_role = UserRole.objects.filter(project=137, site__isnull=False).values("site", "group", "ended_at")
     df_role = pd.DataFrame(list(query_role), columns=["site", "group", "ended_at"])
