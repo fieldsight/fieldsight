b826969599000ab06d7db0f2ad4b7e07933c47b8
awemulya <awemulya@gmail.com>
2020-01-10 16:24:56 +0545

    log serializer of tasks

--- onadata/apps/eventlog/serializers/LogSerializer.py
+++ onadata/apps/eventlog/serializers/LogSerializer.py
@@ -160,3 +160,61 @@ class TaskSerializer(serializers.ModelSerializer):
                         'region_reviewer': project.terms_and_labels.region_reviewer,
                         }
 
+
+class TasklLogSerializer(serializers.ModelSerializer):
+    source_name = serializers.SerializerMethodField(read_only=True)
+    source_img = serializers.ReadOnlyField(source='user.user_profile.profile_picture.url', read_only=True)
+    get_source_url = serializers.ReadOnlyField()
+    get_task_type_display = serializers.ReadOnlyField()
+    get_event_name = serializers.ReadOnlyField()
+    get_event_url = serializers.ReadOnlyField()
+    terms_and_labels = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CeleryTaskProgress
+        exclude = ('content_type', 'object_id', 'user',)
+
+    def get_source_name(self, obj):
+        return obj.user.first_name + " " + obj.user.last_name
+
+    def get_terms_and_labels(self, obj):
+
+        if obj.task_type in [0, 2, 3, 4, 8, 10, 13]:
+
+            if obj.get_event_url():
+                project_id = int(obj.get_event_url().split('/')[5])
+                project = Project.objects.get(id=project_id)
+                terms = ProjectLevelTermsAndLabels.objects.filter(project_id=project.id).exists()
+
+                if terms:
+                    return {'site': project.terms_and_labels.site,
+                            'sub_site': project.terms_and_labels.sub_site,
+                            'donor': project.terms_and_labels.donor,
+                            'site_supervisor': project.terms_and_labels.site_supervisor,
+                            'site_reviewer': project.terms_and_labels.site_reviewer,
+                            'region': project.terms_and_labels.region,
+                            'region_supervisor': project.terms_and_labels.region_supervisor,
+                            'region_reviewer': project.terms_and_labels.region_reviewer,
+                            }
+                else:
+                    return None
+
+        elif obj.task_type == 6:
+            site_id = int(obj.get_event_url().split('/')[5])
+            site = Site.all_objects.get(id=site_id)
+            project = Project.objects.get(id=site.project.id)
+
+            terms = ProjectLevelTermsAndLabels.objects.filter(project_id=project.id).exists()
+
+            if terms:
+
+                return {'site': project.terms_and_labels.site,
+                        'sub_site': project.terms_and_labels.sub_site,
+                        'donor': project.terms_and_labels.donor,
+                        'site_supervisor': project.terms_and_labels.site_supervisor,
+                        'site_reviewer': project.terms_and_labels.site_reviewer,
+                        'region': project.terms_and_labels.region,
+                        'region_supervisor': project.terms_and_labels.region_supervisor,
+                        'region_reviewer': project.terms_and_labels.region_reviewer,
+                        }
+
