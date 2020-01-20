895eb0f5c0a77c6bbd71f8e9ce7f2270f6c88b37
awemulya <awemulya@gmail.com>
2020-01-20 16:04:55 +0545

    status title

--- onadata/apps/fv3/serializers/KoboExportSerializer.py
+++ onadata/apps/fv3/serializers/KoboExportSerializer.py
@@ -3,17 +3,18 @@ from onadata.apps.viewer.models import Export
 
 
 class ExportSerializer(serializers.ModelSerializer):
+    status_title = serializers.SerializerMethodField()
 
     class Meta:
         model = Export
-        fields = ('id', 'filename', 'internal_status', 'created_on', 'status')
-        read_only_fields = ('filename', 'internal_status', 'created_on', 'status')
+        fields = ('id', 'filename', 'internal_status', 'created_on', 'status_title')
+        read_only_fields = ('filename', 'internal_status', 'created_on', 'status_title')
 
-    def get_status(self, obj):
-        if obj.internam_status == 0:
+    def get_status_title(self, obj):
+        if obj.internal_status == 0:
             return "Pending"
-        elif obj.internam_status == 1:
+        elif obj.internal_status == 1:
             return "Successfull"
-        elif obj.internam_status == 2:
+        elif obj.internal_status == 2:
             return "Failed"
 
