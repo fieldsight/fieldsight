6eaa2e05bf296b344907aa782664e8663b21a955
raeesaroj <raesaroj16@gmail.com>
2020-01-22 10:17:34 +0545

    update project geojson commands for specific project.

--- onadata/apps/fieldsight/management/commands/create_geojson.py
+++ onadata/apps/fieldsight/management/commands/create_geojson.py
@@ -5,15 +5,30 @@ from onadata.apps.fieldsight.models import Project, ProjectGeoJSON
 class Command(BaseCommand):
 	help = 'Create site data Geojson file backup for all projects'
 
+	def add_arguments(self, parser):
+		parser.add_argument('project_id', type=int)
+
 	def handle(self, *args, **options):
-		projects = Project.objects.filter(is_active = True)
-		new_projects = 0
-		for project in projects:
+		project_id = options['project_id']
+
+		if project_id != 0:
+			project = Project.objects.get(id=project_id)
 			try:
-				project_jeojson = project.project_geojson
+				project_geojson = project.project_geojson
 			except:
-				project_jeojson = ProjectGeoJSON.objects.create(project_id=project.id)
-			project_jeojson.save()
-			new_projects += 1
-			project_jeojson.generate_new()
-		self.stdout.write('Created "%s " new projects geo data with success!' % new_projects)
+				project_geojson = ProjectGeoJSON.objects.create(project_id=project.id)
+				project_geojson.save()
+			project_geojson.generate_new()
+			self.stdout.write('Created new project geo data with success for "%s "' % project.name)
+		else:
+			projects = Project.objects.filter(is_active=True)
+			new_projects = 0
+			for project in projects:
+				try:
+					project_geojson = project.project_geojson
+				except:
+					project_geojson = ProjectGeoJSON.objects.create(project_id=project.id)
+				project_geojson.save()
+				new_projects += 1
+				project_geojson.generate_new()
+			self.stdout.write('Created "%s " new projects geo data with success!' % new_projects)
