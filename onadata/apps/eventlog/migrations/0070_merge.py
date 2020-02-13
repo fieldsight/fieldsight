472aab1ad40c372be4c4ad78951df9f031869ee5
raeesaroj <raesaroj16@gmail.com>
2020-02-13 15:21:17 +0545

    migrations merge file.

---
+++ onadata/apps/eventlog/migrations/0070_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eventlog', '0064_auto_20200203_1443'),
+        ('eventlog', '0069_auto_20200207_1341'),
+    ]
+
+    operations = [
+    ]
